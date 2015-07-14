3d76090d36557006520cc5d1691e674f2fffcae8
Connor Linfoot <connorlinfoot@me.com>
2015-07-14 20:58:05 +0100

    Removed Updater. Working on a cool update for this ;)

--- src/main/java/com/connorlinfoot/backupmanager/BackupManager.java
+++ src/main/java/com/connorlinfoot/backupmanager/BackupManager.java
@@ -12,11 +12,8 @@ import java.util.Date;
 
 public class BackupManager extends JavaPlugin implements Listener {
     private static BackupManager instance;
-    public static boolean updateAvailable = false;
-    public static String updateMessage = "";
     public static boolean advancedLogs = false;
     public static String location = "backups/";
-    public static String buildNo = "${build.number}";
     public static String pluginPrefix = ChatColor.GRAY + "[" + ChatColor.AQUA + "BackupManager" + ChatColor.GRAY + "] " + ChatColor.RESET;
 
     public void onEnable() {
@@ -28,21 +25,11 @@ public class BackupManager extends JavaPlugin implements Listener {
         advancedLogs = getConfig().getBoolean("Advanced Logs");
         location = getConfig().getString("Backup Location") + "/";
 
-        if (!getConfig().getBoolean("Update Checks")) {
-            getServer().getScheduler().runTaskLaterAsynchronously(this, new Runnable() {
-                public void run() {
-                    checkUpdate(console,
-                            getConfig().getString("Update Branch"),
-                            getConfig().getBoolean("Auto Update"));
-                }
-            }, 10L);
-        }
-
         console.sendMessage("");
         console.sendMessage(ChatColor.BLUE + "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
         console.sendMessage("");
         console.sendMessage(ChatColor.AQUA + getDescription().getName());
-        console.sendMessage(ChatColor.AQUA + "Version " + getDescription().getVersion() + " Build " + buildNo);
+        console.sendMessage(ChatColor.AQUA + "Version " + getDescription().getVersion());
         console.sendMessage("");
         console.sendMessage(ChatColor.BLUE + "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
         console.sendMessage("");
@@ -85,57 +72,4 @@ public class BackupManager extends JavaPlugin implements Listener {
         return instance;
     }
 
-    private void checkUpdate(final ConsoleCommandSender console, final String branch, final boolean install) {
-        if (branch.equalsIgnoreCase("nightly")) {
-            console.sendMessage(ChatColor.GREEN + "Oh, nightly builds? You're brave...");
-            console.sendMessage(ChatColor.RED + "Uh oh, nightly builds are not yet support. Sorry :(");
-        } else if (branch.equalsIgnoreCase("spigot")) {
-            console.sendMessage(ChatColor.GREEN + "Checking for updates via Spigot...");
-            console.sendMessage(ChatColor.RED + "Uh oh, this plugin isn't on Spigot yet. Sorry :(");
-            if (true) return;
-            final Updater updater = new Updater(this, 5018, false);
-            final Updater.UpdateResult result = updater.getResult();
-            switch (result) {
-                default:
-                    break;
-                case BAD_RESOURCEID:
-                case FAIL_NOVERSION:
-                case FAIL_SPIGOT:
-                    updateAvailable = false;
-                    updateMessage = pluginPrefix + "Failed to check for updates. Will try again later.";
-                    getServer().getScheduler().runTaskLaterAsynchronously(this, new Runnable() {
-                        public void run() {
-                            checkUpdate(console,
-                                    branch,
-                                    install);
-                        }
-                    }, 60 * (60 * 20L)); // Checks again an hour later
-                    break;
-                case NO_UPDATE:
-                    updateAvailable = false;
-                    updateMessage = pluginPrefix + "No update was found, you are running the latest version. Will check again later.";
-                    getServer().getScheduler().runTaskLaterAsynchronously(this, new Runnable() {
-                        public void run() {
-                            checkUpdate(console,
-                                    branch,
-                                    install);
-                        }
-                    }, 60 * (60 * 20L)); // Checks again an hour later
-                    break;
-                case DISABLED:
-                    updateAvailable = false;
-                    updateMessage = pluginPrefix + "You currently have update checks disabled";
-                    break;
-                case UPDATE_AVAILABLE:
-                    updateAvailable = true;
-                    updateMessage = pluginPrefix + "An update for CratesPlus is available, new version is " + updater.getVersion() + ". Your installed version is " + getDescription().getVersion() + ".\nPlease update to the latest version :)";
-                    break;
-                case MAJOR_UPDATE_AVALIABLE:
-                    updateAvailable = true;
-                    updateMessage = pluginPrefix + "A major update for CratesPlus is available, new version is " + updater.getVersion() + ". Your installed version is " + getDescription().getVersion() + ".\nPlease update to the latest version :)";
-                    break;
-            }
-            console.sendMessage(updateMessage);
-        }
-    }
 }
